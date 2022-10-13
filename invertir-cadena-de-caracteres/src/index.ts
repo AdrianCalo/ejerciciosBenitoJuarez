99beb6ca5402a5a517956e4e5e6f22bbc629f524
AdrianCalo <110639622+AdrianCalo@users.noreply.github.com>
2022-10-13 20:53:23 -0300

    Add files via upload

---
+++ invertir-cadena-de-caracteres/src/index.ts
@@ -0,0 +1,18 @@
+//let cadena2: string= "tengo que hacer y hacer y hacer muchos ejercicios";
+//console.log(cadena2.length);
+//console.log(cadena2.indexOf("muchos"));
+//console.log(cadena2.lastIndexOf("pollo"));
+//console.log(cadena2.substring(5,15));
+//console.log(cadena2.substr(5,36));
+//console.log(cadena2.toUpperCase());
+//console.log(cadena2.toLowerCase());
+//
+/*-----------------------------------------*/
+let prueva: string = "espejo";
+let pruevaInvertida: string = "";
+//Itero la cadena de manera inversa
+for (let i = prueva.length - 1; i >= 0; i--) {
+  pruevaInvertida += prueva[i];
+}
+console.log(pruevaInvertida);
+/*-----------------------------------------*/
